19553210cca5b3556ee4e82b6910045c03ca6324
Gowtham Rao <gowthamrao@gmail.com>
2020-12-21 19:46:56 -0500

    Added FDA AESI cohorts

--- extras/createCohortDescriptonAndPhenotypeTypeDescriptionFilesFromXLSX.R
+++ extras/createCohortDescriptonAndPhenotypeTypeDescriptionFilesFromXLSX.R
@@ -1,3 +1,9 @@
+phenotypeDescription <- readxl::read_excel(path = "D:\\git\\github\\ohdsi\\PhenotypeLibrary\\extras\\PhenotypeDescription.xlsx") %>%
+  dplyr::mutate(phenotypeName = stringr::str_squish(phenotypeName),
+                clinicalDescription = stringr::str_squish(clinicalDescription))
+
+readr::write_excel_csv(x = phenotypeDescription, file = "D:\\git\\github\\ohdsi\\PhenotypeLibrary\\extras\\PhenotypeDescription.csv", na = "")
+
 phenotypes <- phenotypeDescription %>%
   dplyr::filter(phenotypeId != 0) %>%
   dplyr::filter(is.na(deprecate)) %>%
@@ -14,6 +20,16 @@ for (i in (1:length(phenotypes))) {
   unlink(file.path(basePath, phenotypes[[i]], "phenotypeDescription.csv"), force = TRUE, recursive = TRUE)
   readr::write_excel_csv(x = phenotypeDescriptionLocal, file = file.path(basePath, phenotypes[[i]], "phenotypeDescription.csv"), na = "")
 }
+
+cohortDescription <- readr::read_csv(file = "D:\\git\\github\\ohdsi\\PhenotypeLibrary\\extras\\CohortDescription.csv", col_types = readr::cols(), trim_ws = TRUE)
+
+cohortDescription <- cohortDescription %>%
+  dplyr::mutate(cohortName = stringr::str_squish(cohortName),
+                logicDescription = stringr::str_squish(logicDescription),
+                cohortType = stringr::str_squish(cohortType))
+
+readr::write_excel_csv(x = cohortDescription,
+                       file = "D:\\git\\github\\ohdsi\\PhenotypeLibrary\\extras\\CohortDescription.csv", na = "")
 
 for (i in (1:length(phenotypes))) {
   cohortDescriptionLocal <- cohortDescription %>%
