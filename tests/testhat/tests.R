1a4afe64772a4dce071abb9ea32f998f81c3cd11
Gowtham Rao <rao@ohdsi.org>
2022-07-07 11:29:04 -0400

    Added tests

---
+++ tests/testhat/tests.R
@@ -0,0 +1,18 @@
+testthat::test_that(desc = "Function listPhenotypes", code = {
+  data <- PhenotypeLibrary::listPhenotypes()
+  testthat::expect_true(is.data.frame(data))
+  testthat::expect_gte(object = nrow(data), expected = 1)
+})
+
+testthat::test_that(desc = "Function getPlCohortDefinitionSet", code = {
+  phenotypes <- PhenotypeLibrary::listPhenotypes()
+  cohortDefinitionSet <-
+    PhenotypeLibrary::getPlCohortDefinitionSet(cohortIds = phenotypes[1,]$cohortId)
+  testthat::expect_true(is.data.frame(cohortDefinitionSet))
+  testthat::expect_equal(object = nrow(cohortDefinitionSet),
+                         expected = 1)
+  testthat::expect_equal(
+    object = colnames(cohortDefinitionSet) %>% sort(),
+    expected = c("cohortId", "cohortName", "json", "sql")
+  )
+})
